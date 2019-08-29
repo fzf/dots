17ad656128e002a8ee381fb2e759151197f1dce5
Fletcher Fowler <fletch@fzf.me>
2019-08-29 07:12:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://gitlab.com/fzf/dots.git $HOME/.dots --quiet
 
-for location in $HOME/dots/home/*; do
+for location in $HOME/.dots/home/*; do
   file="${location##*/}"
   echo "Linking '$file' to '$location'"
   ln -sf "$location" "$HOME/.$file"
