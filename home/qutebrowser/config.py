e6ea40946039298f528d15e32ad22ef9be8b10c1
Fletcher Fowler <fletch@fzf.me>
2019-09-12 12:39:15 -0700

    Switch to Monaco font

--- home/qutebrowser/config.py
+++ home/qutebrowser/config.py
@@ -902,7 +902,7 @@ c.fonts.messages.warning = '16pt monospace'
 ## Default monospace fonts. Whenever "monospace" is used in a font
 ## setting, it's replaced with the fonts listed here.
 ## Type: Font
-c.fonts.monospace = '"xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+c.fonts.monospace = 'Monaco'
 
 ## Font used for prompts.
 ## Type: Font
