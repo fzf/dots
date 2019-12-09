3112afea8e86653c0bbb93a4052337464664be75
Fletcher Fowler <fletch@fzf.me>
2019-12-08 23:41:37 -0800

    Adding vim config

--- install/vim.sh
+++ install/vim.sh
@@ -1,2 +1,4 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -sf $HOME/.dots/nvim $HOME/.config/nvim
