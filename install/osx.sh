dc26a04587d990813ad0a5f674091284868dbd46
Fletcher Fowler <fletch@fzf.me>
2023-03-04 21:19:41 -0500

    Karabiner inital commit

--- install/osx.sh
+++ install/osx.sh
@@ -15,6 +15,7 @@ bash $HOME/.dots/install/asdf.sh
 bash $HOME/.dots/install/fzf.sh
 bash $HOME/.dots/install/home.sh
 bash $HOME/.dots/install/hammerspoon.sh
+bash $HOME/.dots/install/karabiner.sh
 bash $HOME/.dots/install/ohmyzsh.sh
 bash $HOME/.dots/install/vim.sh
 bash $HOME/.dots/install/vscode.sh
