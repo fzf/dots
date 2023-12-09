442d5cd18479cf3b625e7a36c103c51fa7259989
Fletcher Fowler <fletch@fzf.me>
2023-12-08 21:36:22 -0500

    Use vscode for shortcuts

--- install/vscode.sh
+++ install/vscode.sh
@@ -1,3 +1,3 @@
-ln -sf ~/.dots/vscode/settings.json /Users/fzf/Library/Application\ Support/Code/User/settings.json
-ln -sf ~/.dots/vscode/keybindings.json /Users/fzf/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/.dots/vscode/settings.json /Users/$USER/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/.dots/vscode/keybindings.json /Users/$USER/Library/Application\ Support/Code/User/keybindings.json
 
