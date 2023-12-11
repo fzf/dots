b931b5d0c9a1b5e161fbea19175014b2fe68a2b8
Fletcher Fowler <fletch@fzf.me>
2023-12-11 03:04:21 +0000

    Add eval for homebrew

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -4,8 +4,10 @@
 which -s brew
 if [[ $? != 0 ]] ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   brew update
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew bundle --file=$HOME/.dots/Brewfile
